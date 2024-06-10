213fa5679181760242145f777edd6bd92d89060d
digrase008 <digrase008@gmail.com>
2024-06-10 08:33:37 +0530

    Initial Commit

---
+++ TrackActivity/TrackActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackActivityApp.swift
+//  TrackActivity
+//
+//  Created by apple on 10/06/24.
+//
+
+import SwiftUI
+
+@main
+struct TrackActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
